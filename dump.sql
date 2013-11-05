206caacb6f7803d773de543bcef0472ea7bd9282
Andrew Williamson <atothendrew24@mac.com>
2013-11-05 16:47:16 -0700

    adding new sql file

---
+++ dump.sql
@@ -0,0 +1,160 @@
+CREATE DATABASE  IF NOT EXISTS `andre124_jeff` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `andre124_jeff`;
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+--
+-- Host: localhost    Database: andre124_jeff
+-- ------------------------------------------------------
+-- Server version	5.6.14
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `cat_id` int(8) NOT NULL AUTO_INCREMENT,
+  `cat_name` varchar(255) NOT NULL,
+  `cat_description` varchar(255) NOT NULL,
+  PRIMARY KEY (`cat_id`),
+  UNIQUE KEY `cat_name_unique` (`cat_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (5,'Politics','Complain about your least favorite party here.'),(6,'Sports','Try to convince everyone (including yourself) that your team is better.'),(7,'America','Obligatory category about how awesome America is. If you disagree, then we disagree with you. ');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+
+--
+-- Table structure for table `otp`
+--
+
+DROP TABLE IF EXISTS `otp`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `otp` (
+  `user_name` varchar(100) NOT NULL,
+  `otp_pass` varchar(100) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE KEY `user_name` (`user_name`,`otp_pass`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `otp`
+--
+
+/*!40000 ALTER TABLE `otp` DISABLE KEYS */;
+/*!40000 ALTER TABLE `otp` ENABLE KEYS */;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `posts` (
+  `post_id` int(8) NOT NULL AUTO_INCREMENT,
+  `post_content` text NOT NULL,
+  `post_date` datetime NOT NULL,
+  `post_topic` int(8) NOT NULL,
+  `post_by` int(8) NOT NULL,
+  PRIMARY KEY (`post_id`),
+  KEY `post_topic` (`post_topic`),
+  KEY `post_by` (`post_by`),
+  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`post_topic`) REFERENCES `topics` (`topic_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`post_by`) REFERENCES `users` (`user_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (42,'^ don\'t need to say much more...','2013-11-05 21:57:40',34,14),(43,'^ don\'t need to say much more...','2013-11-05 21:57:59',35,14),(44,'Why can\'t you just play though the broken collarbone? ','2013-11-05 21:59:24',36,14);
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+
+--
+-- Table structure for table `topics`
+--
+
+DROP TABLE IF EXISTS `topics`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `topics` (
+  `topic_id` int(8) NOT NULL AUTO_INCREMENT,
+  `topic_subject` varchar(255) NOT NULL,
+  `topic_date` datetime NOT NULL,
+  `topic_cat` int(8) NOT NULL,
+  `topic_by` int(8) NOT NULL,
+  PRIMARY KEY (`topic_id`),
+  KEY `topic_cat` (`topic_cat`),
+  KEY `topic_by` (`topic_by`),
+  CONSTRAINT `topics_ibfk_1` FOREIGN KEY (`topic_cat`) REFERENCES `categories` (`cat_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `topics_ibfk_2` FOREIGN KEY (`topic_by`) REFERENCES `users` (`user_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `topics`
+--
+
+/*!40000 ALTER TABLE `topics` DISABLE KEYS */;
+INSERT INTO `topics` VALUES (34,'Republicans are bad!','2013-11-05 21:57:40',5,14),(35,'Democrats are bad!','2013-11-05 21:57:59',5,14),(36,'Aaron Rodgers messed up my fantasy league!','2013-11-05 21:59:24',6,14);
+/*!40000 ALTER TABLE `topics` ENABLE KEYS */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_id` int(8) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(30) NOT NULL,
+  `user_pass` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_date` datetime NOT NULL,
+  `user_level` int(8) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_name_unique` (`user_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (14,'Admin','a94a8fe5ccb19ba61c4c0873d391e987982fbbd3','arwill15@asu.edu','2013-10-01 17:37:39',1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-11-05 23:42:38
